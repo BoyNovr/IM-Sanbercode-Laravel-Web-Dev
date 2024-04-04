6e876032d7ec880f5da4eb31e92247360369b470
BoyNovr <boynovriyal2@gmail.com>
2024-04-04 14:50:34 +0700

    OOP PHP

---
+++ oop/animal.php
@@ -0,0 +1,21 @@
+<?php
+class Animal{
+    protected $name;
+    protected $legs=4;
+    protected $cold_blooded=false;
+
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
+    public function get_name(){
+        return $this->name;
+    }
+    public function get_legs(){
+        return $this->legs;
+    }
+    public function get_cold_blooded(){
+        return $this->cold_blooded? "yes":"no";
+    }
+}
+?>
